43fe35c7ef5fedb308a34cd1dd55764250d86ebe
CiaranHickey5 <ciaran.hickey99@gmail.com>
2025-05-01 13:01:18 +0100

    Create user data class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+    implementation(libs.androidx.room.common.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
